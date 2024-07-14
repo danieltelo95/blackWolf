ae4edc5908bdbc582315a3af4ca4b443a05b16f7
danieltelo95 <daniel.qui.tello@gmail.com>
2024-07-14 07:42:35 -0500

    "feat: install Tailwind CSS and configure for project"

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <button className="px-4 py-2 text-white bg-blue-500 rounded">Click me</button>
     </div>
   );
 }
