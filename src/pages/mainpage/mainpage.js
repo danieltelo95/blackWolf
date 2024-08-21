812999bc58083729f0d3ec67b8edd7e772372eac
danieltelo95 <daniel.qui.tello@gmail.com>
2024-08-21 17:48:26 -0500

    Delete all

--- src/pages/mainpage/mainpage.js
+++ src/pages/mainpage/mainpage.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import Header from '../../components/header/Header';
-import PricingBox from '../../components/Cards/Cards';
-import Title from '../../components/Cards/Title';
-import Testimonials from '../../components/Testimonials/Testimonials';
-import bg from '../../assets/images/bg_1.jpg'; 
-import Footer from '../../components/footer/Footer';
-import About from '../../components/aboutme/About';
-import Courses from '../../components/Courses/Courses';
+
 
 const MainPage = () => {
   return (
-    <div style={{ backgroundImage: `url(${bg})`, minHeight: '100vh', backgroundSize: 'cover' }}>
-      <Header />
-      <Title />
-      <PricingBox />
-      <About />
-      <Courses />
-      <Testimonials />
-      <Footer />
+    <div>
+
     </div>
   );
 };
