5f44313a82224c1c0fc93fa429c0cab46ab0500c
danieltelo95 <daniel.qui.tello@gmail.com>
2024-09-15 22:05:07 -0500

    Fix: Change the header render -Refactor Header component code to show inicio option to authorized or not authorized users

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -41,38 +41,31 @@ const Header = () => {
     return(        
         <div>
             <nav>
-                    {user && (
+                <div className="row-start-2 flex justify-around list-none text-slate-200 mt-4">
+                    <div className="grid grid-cols-4 grid-rows-2 gap-0"></div>
+                    <li><button><Link to="/">Inicio</Link></button></li>
+                    {user ? (
                         <>
-                        <div className="row-start-2 flex justify-around list-none text-slate-200 mt-4">
-                        <div className="grid grid-cols-4 grid-rows-2 gap-0"></div>
-                            <li><button><Link to="/">Inicio</Link></button></li>
-                                {role === 'admin' ? (
-                                    <>
-                                        <li><button className="header-button"><Link to='/cursos'>Cursos</Link></button></li>
-                                        <li><button className="header-button"><Link to='/adminpanel'>Panel de Administrador</Link></button></li>
-                                    </>
-                                ) : (
-                                    <li><button><Link to='/mis-cursos'>Mis Cursos</Link></button></li>
-                                )}
-                            <li><button onClick={handleLogout}>Cerrar sesión</button></li>                                                   
-                        </div>
+                            {role === 'admin' ? (
+                                <>
+                                    <li><button className="header-button"><Link to='/cursos'>Cursos</Link></button></li>
+                                    <li><button className="header-button"><Link to='/adminpanel'>Panel de Administrador</Link></button></li>
+                                </>
+                            ) : (
+                                <li><button><Link to='/mis-cursos'>Mis Cursos</Link></button></li>
+                            )}
+                            <li><button onClick={handleLogout}>Cerrar sesión</button></li>
+                        </>
+                    ) : (
+                        <>
+                            <li><button className="header-button"><Link to='/login'>Iniciar sesión</Link></button></li>
+                            <li><button className="header-button"><Link to='/signup'>Registrarse</Link></button></li>
                         </>
                     )}
-                    {!user && (
-                        <div className="col-span-2 col-start-3 row-start-2 flex justify-between list-none text-slate-200 mt-6 px-12">
-                            <div className="flex space-x-8">
-                                <li><button className="header-button">Sobre mí</button></li>
-                                <li><button><Link to="/">Inicio</Link></button></li>
-                            </div>
-                            <div className="flex space-x-8">
-                                <li><button className="header-button"><Link to='/login'>Iniciar sesión</Link></button></li>
-                                <li><button className="header-button"><Link to='/signup'>Registrarse</Link></button></li>
-                            </div>
-                        </div>               
-                    )}
+                </div>               
             </nav>
         </div>
-    )
+    );
 }
 
 export default Header;
