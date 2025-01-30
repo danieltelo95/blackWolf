bacec8d7fe16dfb496e81eb2e18fe2fdeea7da81
danieltelo95 <daniel.qui.tello@gmail.com>
2025-01-30 15:09:43 -0500

    Style: Responsive header y card detail

--- src/components/Cards/CardDetail.jsx
+++ src/components/Cards/CardDetail.jsx
@@ -1,91 +1,90 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import './CardDetail.css'
+import "./CardDetail.css";
 
 const CardDetail = () => {
+  const { cardName } = useParams();
+  const [card, setCard] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState("");
 
-    const { cardName } = useParams();
-    const [card, setCard] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [modalContent, setModalContent] = useState('');
+  useEffect(() => {
+    const fetchCardDetails = async () => {
+      const url = "https://tarot-api-es.vercel.app/api/v1/cards";
 
-    useEffect(() => {
-      const fetchCardDetails = async () => {
-        const url = 'https://tarot-api-es.vercel.app/api/v1/cards'
-
-        try {
-            const response = await fetch(url)
-            if(!response.ok){
-                throw new Error("Error al obtener los datos");
-            }
-            const data = await response.json();
-            const selectedCard = data.cards.find((card) =>
-                card.name === cardName);
-                setCard(selectedCard);
-        } catch (error) {
-            console.error(error.message);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Error al obtener los datos");
         }
-      }; fetchCardDetails();
-    }, [cardName]);
+        const data = await response.json();
+        const selectedCard = data.cards.find((card) => card.name === cardName);
+        setCard(selectedCard);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchCardDetails();
+  }, [cardName]);
 
-    if(!card) {
-        return <p> Cargando la carta</p>
-    }
+  if (!card) {
+    return <p>Cargando la carta...</p>;
+  }
 
-    const handleOpenModal = (content) => {
-        setModalContent(content);
-        setIsModalOpen(true)
-    }
+  const handleOpenModal = (content) => {
+    setModalContent(content);
+    setIsModalOpen(true);
+  };
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-        setModalContent('');
-    }
-  
-    return (
-        <ul className="grid grid-cols-5 grid-rows-1 mt-20 mb-20">    
-            {/* Columna de la Imagen */}
-            <div className="col-start-2">
-                <div className="detail-card">
-                    <div className="front">
-                        <div className="detail-card-top">
-                        <p className="detail-card-top-para">{card.name}</p>
-                        </div>
-                        <div className="image-container ">
-                        <img
-                            src={card.image}
-                            alt={card.name}
-                            className="detail-card-image"
-                        />
-                        </div>
-                    </div>
-                </div>
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+    setModalContent("");
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center mt-20 mb-20">
+      {/* Imagen de la Carta */}
+      <div className="md:w-1/3 flex justify-center">
+        <div className="detail-card">
+          <div className="front">
+            <div className="detail-card-top">
+              <p className="detail-card-top-para">{card.name}</p>
             </div>
-            <div className="col-start-4">
-                {/* Columna de los Botones */}
-                <div className="buttons-container">
-                    <button onClick={() => handleOpenModal(card.salud)}>Salud</button>
-                    <button onClick={() => handleOpenModal(card.amor)}>Amor</button>
-                    <button onClick={() => handleOpenModal(card.finanzas)}>Finanzas</button>
-                    <button onClick={() => handleOpenModal(card.trabajo)}>Trabajo</button>
-                    <button onClick={() => handleOpenModal(card.espiritualidad)}>
-                        Espiritualidad
-                    </button>
-                </div>
+            <div className="image-container">
+              <img
+                src={card.image}
+                alt={card.name}
+                className="detail-card-image"
+              />
             </div>
-        {isModalOpen && (
-            <div className="modal">
-                <div className="modal-content">
-                    <button className="close-button" onClick={handleCloseModal}>
-                        &times;
-                    </button>
-                    <p>{modalContent}</p>
-                </div>
-            </div>
-        )}
-            
-        </ul>
-    );    
-}
+          </div>
+        </div>
+      </div>
+
+      {/* Contenedor de los botones (Debajo en pantallas pequeñas, al lado en grandes) */}
+      <div className="buttons-container md:w-1/3 md:ml-10">
+        <button onClick={() => handleOpenModal(card.salud)}>Salud</button>
+        <button onClick={() => handleOpenModal(card.amor)}>Amor</button>
+        <button onClick={() => handleOpenModal(card.finanzas)}>Finanzas</button>
+        <button onClick={() => handleOpenModal(card.trabajo)}>Trabajo</button>
+        <button onClick={() => handleOpenModal(card.espiritualidad)}>
+          Espiritualidad
+        </button>
+      </div>
+
+      {/* Modal */}
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <button className="close-button" onClick={handleCloseModal}>
+              &times;
+            </button>
+            <p>{modalContent}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default CardDetail;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,66 +5,104 @@ import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../../firebase/firebase";
 import { clearUser, setUser } from "../../store/reducer/userReducer";
 import { onAuthStateChanged } from "firebase/auth";
-import cursosImage from '../../assets/images/curso-online.png'
-import cartasImage from '../../assets/images/carta-de-tarot.png'
-import andres from '../../assets/images/andres.jpg'
+import cursosImage from "../../assets/images/curso-online.png";
+import cartasImage from "../../assets/images/carta-de-tarot.png";
+import andres from "../../assets/images/andres.jpg";
 import "./Header.css";
 
 const Header = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { user, role } = useSelector((state) => state.user || { user: null, role: null });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user, role } = useSelector((state) => state.user || { user: null, role: null });
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-            if (firebaseUser) {
-                // Usuario autenticado
-                const userRole = firebaseUser.email === 'admin@example.com' ? 'admin' : 'user'; 
-                dispatch(setUser({ user: { uid: firebaseUser.uid, email: firebaseUser.email, displayName: firebaseUser.displayName }, role: userRole }));
-            } else {
-                // Usuario no autenticado
-                dispatch(clearUser());
-            }
-        });
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+      if (firebaseUser) {
+        const userRole = firebaseUser.email === "admin@example.com" ? "admin" : "user";
+        dispatch(
+          setUser({ user: { uid: firebaseUser.uid, email: firebaseUser.email, displayName: firebaseUser.displayName }, role: userRole })
+        );
+      } else {
+        dispatch(clearUser());
+      }
+    });
 
-        return () => unsubscribe(); // Limpia la suscripción al desmontar el componente
-    }, [dispatch]);
+    return () => unsubscribe();
+  }, [dispatch]);
 
-    const handleLogout = async () => {
-        try {
-            await signOut(auth);
-            dispatch(clearUser());
-            navigate('/login')
-        } catch (error) {
-            console.error('Error al cerrar sesión: ', error);
-        }
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      dispatch(clearUser());
+      navigate("/login");
+    } catch (error) {
+      console.error("Error al cerrar sesión: ", error);
     }
+  };
 
-    return(        
-        <header className="sticky top-0 z-50 p-4 bg-transparent">
-            <nav className="flex justify-center space-x-40 items-center mx-auto max-w-7xl px-4">
-                {/* Botón de Cursos */}
-                <Link to="/courses" className="flex items-center gap-2 option-button">
-                    <img src={cursosImage} alt="Cursos" />
-                    <span>Cursos</span>
-                </Link>
+  return (
+    <header className="sticky top-0 z-50 p-4 bg-transparent">
+      <nav className="max-w-7xl mx-auto px-4">
+        <ul className="flex md:flex-row sm:flex-col items-center gap-16 md:gap-8 sm:gap-4">
+          {/* Botón de Cursos */}
+          <li>
+            <Link to="/courses" className="flex items-center gap-2 option-button">
+              <img src={cursosImage} alt="Cursos" className="w-10 h-10 sm:w-8 sm:h-8" />
+              <span className="sm:text-sm">Cursos</span>
+            </Link>
+          </li>
 
-                {/* Imagen de Inicio */}
-                <Link to="/" className="flex items-center justify-center w-12 h-12 rounded-full overflow-hidden border border-white">
-                    <img src={andres} alt="Inicio" className="w-full h-full object-cover transition-transform transform hover:scale-110" />
-                </Link>
+          {/* Imagen de Inicio */}
+          <li>
+            <Link
+              to="/"
+              className="flex items-center justify-center w-12 h-12 sm:w-10 sm:h-10 rounded-full overflow-hidden border border-white"
+            >
+              <img src={andres} alt="Inicio" className="w-full h-full object-cover transition-transform transform hover:scale-110" />
+            </Link>
+          </li>
 
-                {/* Botón de Cartas */}
-                <Link to="/cards" className="flex items-center gap-2 option-button">
-                    <img src={cartasImage} alt="Cartas" />
-                    <span>Cartas</span>
-                </Link>
-            </nav>
-        </header>
-    );
-}
+          {/* Botón de Cartas */}
+          <li>
+            <Link to="/cards" className="flex items-center gap-2 option-button">
+              <img src={cartasImage} alt="Cartas" className="w-10 h-10 sm:w-8 sm:h-8" />
+              <span className="sm:text-sm">Cartas</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
+
+
+        // useEffect(() => {
+        //     const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+        //     if (firebaseUser) {
+        //         const userRole = firebaseUser.email === "admin@example.com" ? "admin" : "user";
+        //         dispatch(
+        //         setUser({ user: { uid: firebaseUser.uid, email: firebaseUser.email, displayName: firebaseUser.displayName }, role: userRole })
+        //         );
+        //     } else {
+        //         dispatch(clearUser());
+        //     }
+        //     });
+
+        //     return () => unsubscribe();
+        // }, [dispatch]);
+
+        // const handleLogout = async () => {
+        //     try {
+        //     await signOut(auth);
+        //     dispatch(clearUser());
+        //     navigate("/login");
+        //     } catch (error) {
+        //     console.error("Error al cerrar sesión: ", error);
+        //     }
+        // };
+
 
 
                 {/* <li><button><Link to="/">Inicio</Link></button></li> */}
